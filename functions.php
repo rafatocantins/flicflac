6e9db17edc86ccac13f867fedbda2da5abc7d1b5
unknown <rafewebdev@gmail.com>
2019-03-22 18:41:47 +0000

    adding files to Theme

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Flic Flac Functions 
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Flic Flac
+ * @since 1.0
+ */
+
+/**
+ * Flic Flac works with wordpress 4.7 ou >
+ */
