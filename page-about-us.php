7244a6423944a6a289ca0f2d15d3e85c21e63463
unknown <rafewebdev@gmail.com>
2019-03-25 20:48:07 +0000

    adding text to header of page about

--- page-about-us.php
+++ page-about-us.php
@@ -1,8 +1,15 @@
 <?php get_header(); ?> 
 
 
-<section id="about-bg">
+<section id="about-bg" class="flic-bg">
+    <h1>Welcome to my studio</h1>
     <img src="<?=get_template_directory_uri() . '/assets/images/background.jpg';?>">
+    <?php for( $i = 1; $i <= 4; $i++){
+    ?>
+        <section class="frame_<?php echo $i; ?>"></section>
+    <?php
+}
+?>
 </section>
 
 <section class="about">
